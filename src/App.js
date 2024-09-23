e8c48580248484b5cfcd5920716a454a6a278617
harishm-git <harishdeepakh@gmail.com>
2024-09-23 21:33:24 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import CanvasDraw from 'react-canvas-draw';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons'; // 
 import './App.css';
 
 function App() {
+  const [brushColor, setBrushColor] = useState("#444");
+  const [brushSize, setBrushSize] = useState(12);
+  const [savedDrawing, setSavedDrawing] = useState(null);
+  const canvasRef = useRef(null);
+
+  const handleColorChange = (e) => {
+    setBrushColor(e.target.value);
+  };
+
+  const handleSizeChange = (e) => {
+    setBrushSize(parseInt(e.target.value, 10));
+  };
+
+  const handleSave = () => {
+    const data = canvasRef.current.getSaveData();
+    setSavedDrawing(data);
+    alert("YOUR DRAWING IS SAVED DONT WORRY!")
+  };
+
+  const handleClear = () => {
+    if (window.confirm("ARE YOU SURE YOU WANT TO CLEAR ALL?")) {
+      canvasRef.current.clear();
+    }
+  };
+  
+
+  const handleLoad = () => {
+    if (savedDrawing) {
+      canvasRef.current.loadSaveData(savedDrawing, true);
+      alert("SUCESSFULLY LOADED !!!")
+    }
+    
+
+  };
+
+  const handleExport = () => {
+    const dataURL = canvasRef.current.getDataURL(); 
+    const link = document.createElement("a");
+    link.href = dataURL;
+    link.download = "drawing.png";
+    link.click();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      <h1 className='chudi'>WhiteBoard</h1>
+      <input 
+        type="color" 
+        value={brushColor} 
+        onChange={handleColorChange}
+        className='inpu'
+      />
+      <div
+        className='brush-size-slider'
+      >
+        <div>
+          <div
+            className='brush-size-slider_2'
+          >
+            <input 
+              type="range" 
+              min="1" 
+              max="50" 
+              value={brushSize} 
+              onChange={handleSizeChange}
+            />
+<label style={{ fontFamily: "Roboto Mono, monospace" }}>Brush Size: {brushSize}</label>
+</div>
+          <span className='expo' onClick={handleExport}>
+            <FontAwesomeIcon className='iconn' icon={faDownload} size='xl'/>
+            Download as PNG
+          </span>
+        </div>
+        <div className="controls">
+          <button className='hehe' onClick={handleSave}>Save Drawing</button>
+          <button className='hehe' onClick={handleLoad}>Load Drawing</button>
+          <button className='hehe' onClick={handleClear}>Clear Board</button>
+        </div>
+      </div>
+      <CanvasDraw
+        ref={canvasRef}
+        brushColor={brushColor}
+        brushRadius={brushSize}
+        canvasWidth={1530}
+        canvasHeight={700}
+      />
     </div>
   );
 }
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
